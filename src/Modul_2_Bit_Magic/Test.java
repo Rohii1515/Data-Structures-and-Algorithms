05155f0b8410e1a1447d629c0dedbfd8a872387a
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-01-05 20:05:23 +0530

    Bit:And

--- src/Modul_2_Bit_Magic/Test.java
+++ src/Modul_2_Bit_Magic/Test.java
@@ -2,7 +2,8 @@ package Modul_2_Bit_Magic;
 
 public class Test {
     public static void main(String[] args) {
-        int x=3,y=6;
+        int x=3,y=6,z=7;
         System.out.println(x&y);
+        System.out.println(x&z);
     }
 }
