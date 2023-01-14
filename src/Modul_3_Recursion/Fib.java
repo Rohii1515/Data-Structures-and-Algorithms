009a7a8606ec9aad4b95594571294faeb8a31166
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-01-14 19:00:47 +0530

    fib

--- src/Modul_3_Recursion/Fib.java
+++ src/Modul_3_Recursion/Fib.java
@@ -7,6 +7,10 @@ public class Fib {
         return 0;
         if(n==1)
         return 1;
+        /*
+         * if(n<=1)
+         * retun n;
+         */
       return fib(n-1)+fib(n-2);
     }
     public static void main(String[] args) {
