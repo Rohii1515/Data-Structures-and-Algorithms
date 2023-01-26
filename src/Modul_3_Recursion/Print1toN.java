4d37cfe88a5897f8cc590c3d2b46b9715ff43fa7
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-01-26 18:27:34 +0530

    Factorial example easy

--- src/Modul_3_Recursion/Print1toN.java
+++ src/Modul_3_Recursion/Print1toN.java
@@ -2,14 +2,15 @@ package Modul_3_Recursion;
 
 public class Print1toN {
     static void print1toN(int n) {
-        if(n==0)
-        return;
-        print1toN(n-1);
-        System.out.println(n);
-        
+        if (n == 0)
+            return;
+        print1toN(n - 1);
+        System.out.print(" " + n);
+
     }
+
     public static void main(String[] args) {
-        int num=5;
+        int num = 5;
         print1toN(num);
     }
 }
