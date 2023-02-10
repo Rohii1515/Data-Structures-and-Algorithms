92d98ea29578d64697480e3a2bd99d696a6cf9d8
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-02-10 15:11:14 +0530

    Reverse Array

--- src/Modul_4_Array/Printing_Reverse_array.java
+++ src/Modul_4_Array/Printing_Reverse_array.java
@@ -1,16 +1,10 @@
 public class Printing_Reverse_array {
 
     public static void printArr(int a[], int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(" " + a[i]);
-        }
-        System.out.println();
-
         for (int i = n - 1; i >= 0; i--) {
             System.out.print(" " + a[i]);
         }
     }
-    //demo
 
     public static void main(String[] args) {
         // int arr[] = new int[5];
