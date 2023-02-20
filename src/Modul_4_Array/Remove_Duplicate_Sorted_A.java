60b923243e83aee75657cda286bad5b5edb237aa
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-02-21 00:38:46 +0530

    Remove Duplicates from sorted array

--- src/Modul_4_Array/Remove_Duplicate_Sorted_A.java
+++ src/Modul_4_Array/Remove_Duplicate_Sorted_A.java
@@ -2,21 +2,28 @@ public class Remove_Duplicate_Sorted_A {
 
     public static int removeDup(int arr[]) {
         int n = arr.length;
-        int res = 1;
+        int rd = 0;
         for (int i = 1; i < n; i++) {
-            if (arr[i] != arr[res - 1]) {
-                arr[res] = arr[i];
-                res++;
+            if (arr[rd] != arr[i]) {
+                rd++;
+                arr[rd] = arr[i];
             }
         }
-        return res;
+        return rd;
     }
 
     public static void main(String[] args) {
         int arr[] = { 10, 20, 20, 30, 30, 30 };
-        int n = removeDup(arr);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
+
+        int rd = removeDup(arr);
+
+        for (int i = 0; i <= rd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 }
