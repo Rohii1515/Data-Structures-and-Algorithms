7c2250a8ece4761e84e4b1d50ba1731e0237f680
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-04-04 12:36:26 +0530

    Simple Bubble Sort Working

---
+++ src/Modul_7_Sorting/Simple_Bubble_Sort.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Simple_Bubble_Sort {
+    public static void bubbleSort(int arr[]) {
+        // outer loop for Turns
+        for (int i = 0; i < arr.length - 1; i++) {
+            // inner loop for checking adjcent elements
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printing(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        int arr[] = { 5, 4, 1, 3, 2 };
+        bubbleSort(arr);
+        printing(arr);
+    }
+}
