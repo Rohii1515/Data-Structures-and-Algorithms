a35ce38c8d177af35cfc84d77013b220ac161dcc
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-04-12 16:05:39 +0530

    Simple program to merge two sorted array and print
    Signed-off-by: Rohii1515 <rohidasjondhale1515@gmail.com>

--- src/Modul_7_Sorting/Merge_Two_Sorted_arrys.java
+++ src/Modul_7_Sorting/Merge_Two_Sorted_arrys.java
@@ -5,27 +5,20 @@ public class Merge_Two_Sorted_arrys {
         int n = b.length;
         int i = 0, j = 0;
         while (i < m && j < n) {
-            if (a[i] <= b[j]) {
-                System.out.print(a[i] + " ");
-                i++;
-            } else {
-                System.out.print(b[j] + " ");
-                j++;
-            }
-            while (i < m) {
-                System.out.print(a[i] + " ");
-                i++;
-            }
-            while (j < n) {
-                System.out.print(b[j] + " ");
-                j++;
-            }
+            if (a[i] <= b[j])
+                System.out.print(a[i++] + " ");
+            else
+                System.out.print(b[j++] + " ");
         }
+        while (i < m)
+            System.out.print(a[i++] + " ");
+        while (j < n)
+            System.out.print(b[j++] + " ");
     }
 
     public static void main(String[] args) {
-        int a[] = { 10, 20, 50 };
-        int b[] = { 5, 50, 50 };
+        int a[] = { 10, 15, 20, 40 };
+        int b[] = { 5, 6, 6, 10, 15 };
         merge(a, b);
     }
 }
